d1a19ea19d28d7b4a1bca93fd48eefbd06f29c62
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-10-05 10:02:12 +0600

    add color constant

---
+++ lib/view/grocery_on_board.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/utils/color_utils.dart';
+
+class GroceryOnBoard extends StatelessWidget {
+  const GroceryOnBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          ClipPath(
+            child: Container(
+              width: size.width,
+              height: size.height * 0.6,
+              color: textGreen,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
