30b9128d79b6812a319641c1b7f1790f4056880e
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-10-20 08:13:56 +0600

    bottomNavBar done

---
+++ lib/app/widget/bottom_nav_bar.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import '../module/home/view/home_screen_grocery.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int currentIndex = 0;
+
+  final List<Widget> pages = [
+    HomeScreenGrocery(),
+    Center(
+      child: Text("Data"),
+    ),
+    HomeScreenGrocery(),
+    Center(
+      child: Text("Maile"),
+    ),
+    Center(
+      child: Text("Profile"),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: pages[currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.green,
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          currentIndex: currentIndex,
+          onTap: (value) {
+            setState(() {
+              currentIndex = value;
+            });
+          },
+          items: [
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.home_rounded), label: ""),
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.insert_chart), label: ""),
+            BottomNavigationBarItem(
+                icon: Container(
+                  padding: const EdgeInsets.all(15),
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.green),
+                  child: const Icon(
+                    Icons.qr_code_scanner,
+                    color: Colors.white,
+                  ),
+                ),
+                label: ""),
+            const BottomNavigationBarItem(icon: Icon(Icons.mail), label: ""),
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline), label: ""),
+          ],
+        ));
+  }
+}
